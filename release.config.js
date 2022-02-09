62f858698ae482d0416af2ab1b01158e59bf5770
Future Dominique <77439365+pfuture17@users.noreply.github.com>
2022-02-09 22:14:31 +0800

    fix:fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/pfuture17/react-app",
+  plugin: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/git",
+  ],
+};
